f89c12207d28ec40dd61a8919725ff0b2c2ba862
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-11-13 23:33:45 -0600

    feature requests are working

--- scraper.py
+++ scraper.py
@@ -1,30 +1,34 @@
-# from playwright.sync_api import sync_playwright
-# from bs4 import BeautifulSoup
-#
-# def main():
-#     with sync_playwright() as playwright:
-#         # Open chrome and navigate to target page
-#         browser = playwright.chromium.launch(headless=False)
-
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-url = "https://en.wikipedia.org/wiki/History_of_Mexico"
-response = requests.get(url)
-
-soup = BeautifulSoup(response.content, "html.parser")
-
-missing_citations = soup.find_all(lambda tag: tag.name == 'p' and tag.find('a', href="/wiki/Wikipedia:Citation_needed") is not None)
-
-print(missing_citations)
+url = "https://en.wikipedia.org/wiki/Laos"
 
 
+def get_citations_needed_count(url_address: str) -> int:
+    response = requests.get(url_address)
+    soup = BeautifulSoup(response.content, "html.parser")
+    # https://chat.openai.com/c/67fb1e11-a1bc-40dc-9410-71b1f27933a2
+    missing_citations = soup.find_all(
+        lambda tag: tag.name == 'p' and tag.find('a', href="/wiki/Wikipedia:Citation_needed") is not None)
+    count = 0
+    # https://chat.openai.com/c/4eac8099-d441-4e4e-880c-1715b3990bd3
+    for citation in missing_citations:
+        if "citation needed" in citation.text:
+            count += 1
+    return count
 
 
+def get_citations_needed_report(url_address: str) -> str:
+    response = requests.get(url_address)
+    report = ""
+    soup = BeautifulSoup(response.content, "html.parser")
+    missing_citations = soup.find_all(
+        lambda tag: tag.name == 'p' and tag.find('a', href="/wiki/Wikipedia:Citation_needed") is not None)
+    for citation in missing_citations:
+        if "citation needed" in citation.text:
+            report += f'{citation.text}\n'
+    return report
 
 
-
-
-#
-# if __name__ == "__main__":
-#     main()
+print(get_citations_needed_count(url))
+print(get_citations_needed_report(url))
